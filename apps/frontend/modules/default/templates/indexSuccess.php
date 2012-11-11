95725df53809cb7b743391baf0f619c8cff2d615
Yannick Mahe <mahe.yannick@gmail.com>
2012-11-11 13:20:57 +0100

    js

--- apps/frontend/modules/default/templates/indexSuccess.php
+++
@@ -1,1 +0,0 @@
-<h1>It works!<i class="icon-pencil"></i><i class="icon-bar-chart"></i></h1>
--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -7,8 +7,30 @@
     <link rel="shortcut icon" href="/favicon.ico" />
     <?php include_stylesheets() ?>
     <?php include_javascripts() ?>
+    <link rel="stylesheet/less" type="text/css" href="less/stub.less">
+    <script src="js/less-1.3.1.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+
+      <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+      <!--[if lt IE 9]>
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+      <![endif]-->
   </head>
   <body>
     <?php echo $sf_content ?>
+    <!-- Bootstrap Javascript -->
+    <script src="bootstrap/js/bootstrap-affix.js"></script>
+    <script src="bootstrap/js/bootstrap-alert.js"></script>
+    <script src="bootstrap/js/bootstrap-button.js"></script>
+    <script src="bootstrap/js/bootstrap-carousel.js"></script>
+    <script src="bootstrap/js/bootstrap-collapse.js"></script>
+    <script src="bootstrap/js/bootstrap-dropdown.js"></script>
+    <script src="bootstrap/js/bootstrap-modal.js"></script>
+    <script src="bootstrap/js/bootstrap-scrollspy.js"></script>
+    <script src="bootstrap/js/bootstrap-tab.js"></script>
+    <script src="bootstrap/js/bootstrap-tooltip.js"></script>
+    <script src="bootstrap/js/bootstrap-popover.js"></script>
+    <script src="bootstrap/js/bootstrap-transition.js"></script>
+    <script src="bootstrap/js/bootstrap-typeahead.js"></script>
   </body>
 </html>
